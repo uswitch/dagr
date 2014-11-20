a42485d256f7af660969413821c1a43cd5d3e6b2
Richard Lewis Jones <rlj@uswitch.com>
2014-11-20 14:29:18 +0000

    templates and resource packaging

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ dagr
 ====
 
 runs programs every day (in Norse mythology, Dagr is day personified)
+
+## installation
+
+Install dagr
+    go get -u github.com/uswitch/dagr
+
+Install resource packaging tool
+
+    go get -u bitbucket.org/tebeka/nrsc/nrsc
+
+Package dagr with its resources
+
+    nrsc $GOPATH/bin/dagr $GOPATH/src/github.com/uswitch/dagr/resources
+
+Now, $GOPATH/bin/dagr can be copied anywhere and run.
--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"bitbucket.org/tebeka/nrsc"
 	"gopkg.in/alecthomas/kingpin.v1"
 	"log"
 	"net/http"
+	"regexp"
 )
 
-var httpAddr = kingpin.Flag("http", "serve http on host:port").Default(":7730").Short('a').Required().TCP()
-var tasksRepo = kingpin.Flag("repo", "repository containing tasks").Short('r').Required().String()
-var workingDir = kingpin.Flag("work", "working directory").Short('w').Required().String()
-var dbFile = kingpin.Flag("db", "sqlite database file").Short('d').Required().String()
+var TMPL = regexp.MustCompile(".tmpl$")
+
+var httpAddr = kingpin.Flag("http", "serve http on host:port").Short('a').Required().TCP()
+
+//var tasksRepo = kingpin.Flag("repo", "repository containing tasks").Short('r').Required().String()
+//var workingDir = kingpin.Flag("work", "working directory").Short('w').Required().String()
+//var dbFile = kingpin.Flag("db", "sqlite database file").Short('d').Required().String()
+
+type SomeVariables struct {
+	Answer int
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	t, err := nrsc.LoadTemplates(nil, "index.html.tmpl")
+	if err != nil {
+		http.NotFound(w, req)
+	}
+	if err = t.Execute(w, SomeVariables{42}); err != nil {
+		http.NotFound(w, req)
+	}
+}
 
 func main() {
 	kingpin.Parse()
+
+	nrsc.Handle("/static/")
+	nrsc.Mask(TMPL)
+	http.HandleFunc("/", indexHandler)
 
 	server := &http.Server{
 		Addr: httpAddr.String(),
