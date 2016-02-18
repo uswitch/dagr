8e1830d0ade74e29186b226780cea9ad2ca22c60
Siddharth Dawara <siddharthdawara@gmail.com>
2016-02-18 10:26:57 +0000

    reverting assumption that bash is available on the host machine

--- program/program.go
+++ program/program.go
@@ -59,7 +59,7 @@ func (p *Program) Execute(startCh <-chan bool, ch chan<- ExitCode) (*Execution, 
 	defer p.Unlock()
 
 	ProgramLog(p, "executing command")
-	cmd := exec.Command("bash", p.CommandPath)
+	cmd := exec.Command(p.CommandPath)
 	cmd.Dir = filepath.Dir(p.CommandPath)
 	cmd.Env = os.Environ()
 	ProgramLog(p, fmt.Sprintf("environment: %s", os.Environ()))
